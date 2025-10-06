07e7b84625f6894cfd9c723d157fcc7af5342f41
claim17 <147082704+claim17@users.noreply.github.com>
2025-10-06 17:51:10 +0200

    Add files via upload

---
+++ main/Window1.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prueba
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string User = this.txtUsuario.Text;
+            string Password = this.txtPassword.Password;
+
+            if (User == "" || Password == "")
+            {
+                MessageBox.Show("Inserte el nombre del usuario o el Password");
+                this.txtUsuario.Focus();
+                return;
+            }
+            else
+            {
+                if (txtUsuario.Text != "Alumno" || txtPassword.Password != "1234")
+                {
+                    MessageBox.Show("El nombre de usuario o el password es incorrecto");
+                    this.txtUsuario.Focus();
+                    return;
+                }
+                else
+                    MessageBox.Show("Usuario correcto");
+                return;
+            }
+        }
+
+        private void Rectangle_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            Window Login = new Window3();
+            Login.Show();
+        }
+    }
+}
---
+++ main/Window3.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prueba
+{
+    /// <summary>
+    /// Lógica de interacción para Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Window Login = new Window1();
+            Login.Show();
+        }
+
+       
+    }
+}
